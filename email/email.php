a8f7fb01c73830f11ecd46d30cf2c11d057c05b3
Amanda Brito <112962708+amandalbrito@users.noreply.github.com>
2024-01-31 17:55:00 -0300

    Upload PHP

---
+++ email/email.php
@@ -0,0 +1,24 @@
+<?php
+
+    if(isset($_POST['email']) && !empty($_POST['email']));
+
+    $nome = addcslashes($_POST['nome']);
+    $email = addcslashes($_POST['email']);
+    $assunto = addcslashes($_POST['asssunto']);
+    $mensagem = addcslashes($_POST['mensagem']);
+
+$to = "amandaluciabrito73@gmail.com";
+$subject = "Conatato- Iportfolio";
+$body = "Nome: ".$nome. "\n".
+        "Email: ".$email. "\n".
+        "Assunto: ".$assunto. "\n".
+        "Mensagem: ".$mensagem;
+
+$header = "From: amandaluciabrito73@gmail.com" "\r\n"."Reply-To: ".$email. "\r\n" ."X=Mailer:PHP/" .phpversion();
+
+if(mail($to,$subject,$header)){
+    echo("Email enviado com sucesso");
+}else{
+    echo("O emial não pode ser enviada");
+}
+?>
