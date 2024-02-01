f3d07fef1ad642ba8d55e96f70b12a4089a9ead1
Amanda Brito <112962708+amandalbrito@users.noreply.github.com>
2024-02-01 09:15:53 -0300

    Update PHP

--- email/email.php
+++ email/email.php
@@ -1,24 +1,24 @@
-<?php
+<?php 
 
-    if(isset($_POST['email']) && !empty($_POST['email']));
+        if(isset($_POST['email']) && !empty($_POST['email']));
 
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
-    $assunto = addcslashes($_POST['asssunto']);
-    $mensagem = addcslashes($_POST['mensagem']);
+        $nome = addcslashes($_POST['nome']); //Variável para recolher informações. adsslashes é para não ter erros ao enviar
+        $email = addcslashes($_POST['email']);
+        $assunto = addcslashes($_POST['asssunto']);
+        $mensagem = addcslashes($_POST['mensagem']);
 
-$to = "amandaluciabrito73@gmail.com";
-$subject = "Conatato- Iportfolio";
-$body = "Nome: ".$nome. "\n".
-        "Email: ".$email. "\n".
+    $to = "amandaluciabrito73@gmail.com"; // Para onde será enviado
+    $assunto = "Conatato- Iportfolio"; //$ + variável 
+    $body = "Nome: ".$nome. "\n". //.= Concatenação= unificar duas informações 
+        "Email: ".$email. "\n". //quebra linha 
         "Assunto: ".$assunto. "\n".
         "Mensagem: ".$mensagem;
 
-$header = "From: amandaluciabrito@hotmail.com" "\r\n"."Reply-To: ".$email. "\r\n" ."X=Mailer:PHP/" .phpversion();
+    $header = "From: amandaluciabrito@hotmail.com" "\r\n"."Reply-To: ".$email. "\r\n" ."X=Mailer:PHP/" .phpversion(); //."X=Mailer:PHP/" .phpversion() versão php
 
-if(mail($to,$subject,$header)){
-    echo("Email enviado com sucesso");
-}else{
-    echo("O emial não pode ser enviada");
-}
+    if(mail($to,$assunto,$body,$header)){ //Valor verdadeiro
+        echo("Email enviado com sucesso");
+    }else{ //Valor falso
+        echo("Houve um erro ao enviar o emial ");
+    }
 ?>
