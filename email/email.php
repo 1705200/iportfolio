372dbed0925ca81f055ec5cf4044b03d46127737
Amanda Brito <112962708+amandalbrito@users.noreply.github.com>
2024-02-01 10:14:12 -0300

    Update email.php

--- email/email.php
+++ email/email.php
@@ -1,7 +1,5 @@
 <?php 
 
-        if(isset($_POST['email']) && !empty($_POST['email']));
-
         $nome = addcslashes($_POST['nome']); //Variável para recolher informações. adsslashes é para não ter erros ao enviar
         $email = addcslashes($_POST['email']);
         $assunto = addcslashes($_POST['asssunto']);
@@ -9,12 +7,12 @@
 
     $to = "amandaluciabrito73@gmail.com"; // Para onde será enviado
     $assunto = "Conatato- Iportfolio"; //$ + variável 
-    $body = "Nome: ".$nome. "\n". //.= Concatenação= unificar duas informações 
-        "Email: ".$email. "\n". //quebra linha 
-        "Assunto: ".$assunto. "\n".
+    $body = "Nome: ".$nome ."\n"; //.= Concatenação= unificar duas informações 
+        "Email: ".$email ."\n"; //quebra linha 
+        "Assunto: ".$assunto ."\n";
         "Mensagem: ".$mensagem;
 
-    $header = "From: amandaluciabrito@hotmail.com" "\r\n"."Reply-To: ".$email. "\r\n" ."X=Mailer:PHP/" .phpversion(); //."X=Mailer:PHP/" .phpversion() versão php
+    $header = "From: amandaluciabrito@hotmail.com" ."\r\n" ."Reply-To: ".$email ."\r\n" ."X=Mailer:PHP/" .phpversion(); //."X=Mailer:PHP/" .phpversion() versão php
 
     if(mail($to,$assunto,$body,$header)){ //Valor verdadeiro
         echo("Email enviado com sucesso");
